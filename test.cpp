dba95af38aa3c2f62f651c7f26f576260043e583
Justin D Forman <jforman@unix4.andrew.cmu.edu>
2017-11-24 17:51:03 -0500

    Column tester

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 //CPP Packages
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <string.h>
 
 //User defined libraries
 #include "inc/column.h"
+#include "inc/testcolumn.h"
 
 using namespace std;
 
@@ -49,6 +52,106 @@ int main() {
 
     cout << "Goodbye!" << std::endl;
 
+    Test_Column tc(0);
+    tc.random_test(10, 0, 0);
+
     return 0;
+
+}
+
+/*****************************************************************************
+ *                             TEST_COLUMN                                   *
+ *****************************************************************************/
+
+Test_Column::Test_Column(int comp_opt) {
+    _col = new Column("dut", comp_opt);
+}
+
+Test_Column::~Test_Column() {
+    delete _col;
+}
+
+
+int Test_Column::random_test(int num_small, int num_med, int num_large) {
+
+    int ssize = 100;
+    int msize = 1000;
+    int lsize = 10000;
+
+    char small_buf [ssize];
+    char med_buf   [msize];
+    char large_buf [lsize]; //larger than page size
+
+    int remaining_small = num_small;
+    int remaining_med   = num_med;
+    int remaining_large = num_large;
+
+    string vstr;
+    string kstr;
+
+    //Write all values into test column
+    while((remaining_small + remaining_med + remaining_large) > 0) {
+
+        switch(rand() % 3) {
+            case 0: //small value, med key
+                generate_rand_string(small_buf, ssize);
+                generate_rand_string(med_buf, ssize);
+                vstr = small_buf;
+                kstr = med_buf;
+                remaining_small--;
+                break;
+
+            case 1: //med value, small key
+                generate_rand_string(small_buf, ssize);
+                generate_rand_string(med_buf, ssize);
+                kstr = small_buf;
+                vstr = med_buf;
+                remaining_med--;
+                break;
+
+            default: //large value, med key
+                generate_rand_string(large_buf, lsize);
+                generate_rand_string(med_buf, msize);
+                kstr = med_buf;
+                vstr = large_buf;
+                remaining_large--;
+                break;
+        }
+
+        _col->write(kstr, vstr);
+        _map[kstr] = vstr;
+
+    }
+
+    //Read all values from test column
+    _iter = _map.begin();
+    while(_iter != _map.end()) {
+        vstr = _col->read(_iter->first);
+        if(vstr != _iter->second) {
+            cout << "Test Failed!" << std::endl;
+            cout << "Actual: " << _iter->second << " Observed: " << vstr << std::endl;
+            return -1;
+        }
+
+    }
+
+    cout << "Test success" << std::endl;
+    return 0;
+
+}
+
+void Test_Column::generate_rand_string(char *buf, int size) {
+
+    int  i;
+    char r;
+
+    memset(buf, '\0', size);
+
+    for(i = 0; i < (size-1); i++) {
+        r = (char) (rand() % 126 + 33); //Useful ascii chars are 33-126
+        memset((buf + i), r, 1);
+    }
+
+    return;
 
 }
